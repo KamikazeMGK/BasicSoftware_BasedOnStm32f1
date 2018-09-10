f51b9cae11f23b52b680e8f253cb95d5ac4c00e0
KamikazeMGK <=>
2018-09-10 23:22:51 +0700

    Clock configuration is added for stm32f1 mcu

---
+++ 3_MCU_Drivers/1_Clock_Configuration/Clock_Config.h
@@ -0,0 +1,20 @@
+/*****************************************************************
+ *  @brief MCU Clock for stm32f103c8tc
+ *         This configuration is generate by CubeMX
+ *		   Header file provide interface to uppper layer
+ *  @author Bui Duong
+ *  @date	10/9/2018
+ *
+ *
+ * ****************************************************************/
+#ifndef _MCU_CLOCKCONGIG_H
+#define _MCU_CLOCKCONGIG_H
+
+
+
+
+
+
+extern void SystemClock_Config();
+
+#endif /*_MCU_CLOCKCONGIG_H*/
